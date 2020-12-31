a1f58af74696e0e973d693ed7c812d22ae9ad3cc
revanth994 <krevanthn@gmail.com>
2020-12-31 15:02:19 +0530

    checking errors

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,23 @@
-
 #!/bin/bash
-
 echo "welcome to Employee Wage Computation"
-
 ispresent=1;
-randomcheck=$((RANDOM%2))
-
+randomcheck=$((RANDOM%3))
+isFulltime=1
+isParttime=2
+empwagePrHr=20
+case  $empcheck in
 if [ $ispresent -eq $randomcheck ];
- then
-        echo "Employee is present"
-else
-        echo "Employee is Absent"
+	echo "Employee is present"
+	$isFulltime)
+		empHrs=8
+		;;
+	$isParttime)
+		empHrs=4
+		;;
+	*)
+		empHrs=0
+		;;
+	echo "Employee is absent"
 fi
-
+esac
+salary=$(( $empHrs * $empwagePrHr ))
